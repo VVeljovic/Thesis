095f4989068c753e6ae51adfbc66ec6288843316
VVeljovic <veljko.veljovic13@gmail.com>
2024-07-15 20:36:45 +0200

    start

---
+++ UserService/UserService/DbContext/DBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UserService.Models;
+namespace UserService.DbContext
+{
+    public class DBContext : IdentityDbContext<User>
+    {
+        public DBContext(DbContextOptions<DBContext> options) : base(options) { }
+       
+    }
+}
