27969747f3218ffdfccc3d00dbdcd6f5ca728b60
VVeljovic <veljko.veljovic13@gmail.com>
2024-07-15 20:42:08 +0200

    User Service

---
+++ UserService/UserService/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UserService.Controllers
+{
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public String Get()
+        {
+            return "Nall";
+        }
+    }
+}
